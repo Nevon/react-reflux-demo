e3a440db2c63cfbe5766e32d2a4653488f6645c9
Tommy Brunn <tommy.brunn@gmail.com>
2015-01-22 10:50:07 +0100

    Added some basic Jest tests

---
+++ app/scripts/lib/__tests__/cursor.js
@@ -0,0 +1,39 @@
+jest.dontMock('../cursor');
+var Cursor = require('../cursor');
+
+describe('Cursor', function() {
+	it('gets you the current item', function() {
+		var arr = ['first', 'second', 'third'];
+		var c = new Cursor(arr);
+
+		expect(c.current()).toBe('first');
+	});
+
+	it('gets you sequential items', function() {
+		var arr = ['first', 'second', 'third'];
+		var c = new Cursor(arr);
+
+		expect(c.next()).toBe('second');
+		expect(c.next()).toBe('third');
+	});
+
+	it('wraps around the array', function() {
+		var arr = ['first', 'second'];
+		var c = new Cursor(arr);
+
+		expect(c.next()).toBe('second');
+		expect(c.next()).toBe('first');
+		expect(c.prev()).toBe('second');
+	});
+
+	it('maintains the correct index going backwards and forwards', function() {
+		var arr = ['first', 'second', 'third'];
+		var c = new Cursor(arr);
+
+		expect(c.getIndex()).toBe(0);
+		c.next();
+		expect(c.getIndex()).toBe(1);
+		c.prev();
+		expect(c.getIndex()).toBe(0);
+	});
+});
